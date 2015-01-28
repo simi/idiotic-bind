e3ca635f3d43a5ae35fc1a9a863bbaa75b3bf3b1
Josef Šimánek <josef.simanek@gmail.com>
2015-01-28 18:28:44 +0100

    Style tweaks in main.js.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,38 @@
-function User( uid ) {
-  var binder = new DataBinder( uid ),
+function User(uid) {
+  var binder = new DataBinder(uid),
 
-  user = {
-    attributes: {
-      firstname: "Josef",
-      lastname: "Šlendr",
-      fullname: function() {
+  var user = {
+    'attributes': {
+      'firstname': "Josef",
+      'lastname': "Šlendr",
+      'fullname': function() {
         return user.attributes.firstname + ' ' + user.attributes.lastname;
       }
     },
 
-    // The attribute setter publish changes using the DataBinder PubSub
-    set: function( attr_name, val ) {
-      this.attributes[ attr_name ] = val;
+    'set': function(attr_name, val) {
+      this.attributes[attr_name ] = val;
       this.refreshAll();
     },
 
-    get: function( attr_name ) {
-      return this.attributes[ attr_name ].call();
+    'get': function(attr_name) {
+      return this.attributes[attr_name].call();
     },
 
-    refreshAll: function() {
-      for(var key in this.attributes) {
-        binder.trigger( uid + ":change", [ key, this.attributes[key], this ] );
+    'refreshAll': function() {
+      var key;
+      for(key in this.attributes) {
+        binder.trigger(uid + ":change", [key, this.attributes[key], this]);
       }
     },
 
-    _binder: binder
+    '_binder': binder
   };
 
   // Subscribe to the PubSub
-  binder.on( uid + ":change", function( evt, attr_name, new_val, initiator ) {
-    if ( initiator !== user ) {
-      user.set( attr_name, new_val );
+  binder.on( uid + ":change", function(evt, attr_name, new_val, initiator) {
+    if (initiator !== user) {
+      user.set(attr_name, new_val);
     }
   });
 
@@ -43,6 +43,6 @@ function User( uid ) {
 }
 
 // javascript
-var user = new User( 'user123' );
+var user = new User('user123');
 // user.set( "firstname", "Josef" );
 // user.set( "lastname", "Šlendr" );
